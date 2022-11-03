99f95393642475723b7b553f192e2490777e0e59
kevinbomb <kevinwomb@gmail.com>
2022-11-03 17:00:08 +0700

    accelerometer & notif

--- app/src/main/java/com/gd9/ugd89_c_10547_project1/MainActivity.kt
+++ app/src/main/java/com/gd9/ugd89_c_10547_project1/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.gd9.ugd89_c_10547_project1
 
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.hardware.*
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,15 +16,21 @@ import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import java.lang.Exception
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private var mCamera: Camera? = null
     private var mCameraView: CameraView? = null
     private var camInfo= Camera.CameraInfo.CAMERA_FACING_BACK
     lateinit var proximitySensor: Sensor
     lateinit var sensorManager: SensorManager
+
+    private val CHANNEL_ID_1 = "channel_notification_01"
+    private val notificationId1 = 101
+
     var proximitySensorEventListener: SensorEventListener? = object : SensorEventListener {
         override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
             //
@@ -49,13 +61,95 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
+            if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+                //Log.d("Main", "onSensorChanged: sides ${event.values[0]} front/back ${event.values[1]} ")
+
+                // Sides = Tilting phone left(10) and right(-10)
+                val sides = event.values[0]
+
+                // Up/Down = Tilting phone up(10), flat (0), upside-down(-10)
+                val upDown = event.values[1]
+
+                // Changes the colour of the square if it's completely flat
+                if (upDown.toInt() != 0 && sides.toInt() != 0) {
+                    sendNotification()
+                }
+            }
+        }
+    }
+
+    private fun setUpSensorStuff() {
+        // Create the sensor manager
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        // Specify the sensor you want to listen to
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        // Checks for the sensor we have registered
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            //Log.d("Main", "onSensorChanged: sides ${event.values[0]} front/back ${event.values[1]} ")
+
+            // Sides = Tilting phone left(10) and right(-10)
+            val sides = event.values[0]
+
+            // Up/Down = Tilting phone up(10), flat (0), upside-down(-10)
+            val upDown = event.values[1]
+
+
+            // Changes the colour of the square if it's completely flat
+            if (upDown.toInt() > 2 || sides.toInt() > 2){sendNotification()}
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
+    }
+
+
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Modul89_A_10547"
+            val descriptionText = "Notification Description"
+
+            val channel1 = NotificationChannel(CHANNEL_ID_1, name, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel1)
+        }
+    }
+
+    private fun sendNotification(){
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID_1)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Modul89_C_10547")
+            .setContentText("Hape Anda Goyang~~")
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationId1, builder.build())
         }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
-
+        setUpSensorStuff()
+        createNotificationChannel()
         setContentView(R.layout.activity_main)
         try {
             mCamera = Camera.open(0)
